cc63c3996c48ab5019a686b696a3ff74369576ad
Nedu Robert <nedumrobert@gmail.com>
2018-05-28 23:02:45 +0100

    setup initial server

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const server = express();
+const corsOptions = {
+  origin: 'https://lambda-notes-app.netlify.com/',
+  credentials: true,
+};
+
+// const userRoutes = require('./routes/users');
+// const noteRoutes = require('./routes/notes');
+
+const url = process.env.DATABASE_URL || 'mongodb://localhost/lambdanotesdb';
+mongoose.connect(url)
+.then(mongo => {
+  console.log(`Sucessfully connected to database`)
+})
+.catch(err => {
+  console.log(`Error connecting to database: ${err}`)
+})
+
+// Middlewares
+server.use(cors(corsOptions));
+server.use(helmet());
+server.use(express.json());
+
+// Route Handlers
+// server.use('/api/', authRoutes);
+// server.use('/api/', noteRoutes);
+
+const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'test') {
+    server.listen(port, () => console.log(`\n=== API running on port ${port} ===\n`));
+}
+
+module.exports = server;
