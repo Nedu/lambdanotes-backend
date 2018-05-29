58815ba713d3625b446cd729197d2da855bac56d
Nedu Robert <nedumrobert@gmail.com>
2018-05-29 01:29:17 +0100

    created schema for Note model

---
+++ api/models/Note.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const Note = new mongoose.Schema({
+    author: {
+        type: ObjectId,
+        ref: User,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: Array
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Note', Note);
