20873a4a8465d5b022660ef820663b63a1f7e60d
Nedu Robert <nedumrobert@gmail.com>
2018-09-27 17:58:51 +0100

    Improved register and login controllers

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -20,33 +20,41 @@ exports.findOne = (req, res) => {
 // Register
 exports.register = (req, res) => {
     const { username, password } = req.body;
-    const newUser = { username, password };
-    const user = new User(newUser);
-    user.save().then(user => {
-        const token = makeToken(user);
-        res.status(201).json({ user, token });
+    User.findOne({ username }).then(existingUser => {
+        if(existingUser) {
+            return res.status(409).json({ message: 'problem signing up'});
+        } else {
+            const user = new User({ username, password });
+            user.save().then(user => {
+                const token = makeToken(user);
+                res.status(201).json({ token });
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
+        }
     })
-    .catch(err => {
-        res.status(500).json(err);
-    })    
 };
 
 // Login
 exports.login = (req, res) => {
+    if(!req.user) {
+        return res.status(422);
+    }
     res.status(200).json({ token: makeToken(req.user), user: req.user })
 }
 
-// Logout
-exports.logout = (req, res) => {
-    if (req.session) {
-        req.session.destroy(err => {
-            if (err) res.status(500).json({ error: 'error logging out' });
-            else res.status(200).send('Logged out');
-        });
-    } else {
-        res.send('User not logged in');
-    }
-}
+// // Logout
+// exports.logout = (req, res) => {
+//     if (req.session) {
+//         req.session.destroy(err => {
+//             if (err) res.status(500).json({ error: 'error logging out' });
+//             else res.status(200).send('Logged out');
+//         });
+//     } else {
+//         res.send('User not logged in');
+//     }
+// }
 
 // Update an exisiting user
 exports.update = (req, res) => {
