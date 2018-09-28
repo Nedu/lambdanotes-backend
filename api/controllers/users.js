064de9b3b568b13fa1b0eddafcd4b67193b96b5a
Nedu Robert <nedumrobert@gmail.com>
2018-09-28 23:51:55 +0100

    Cleaned up users controllers

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -12,11 +12,6 @@ exports.findAll = (req, res) => {
     });
 };
 
-// Retrieve an existing user
-exports.findOne = (req, res) => {
-
-};
-
 // Register
 exports.register = (req, res) => {
     const { username, password } = req.body;
@@ -27,7 +22,7 @@ exports.register = (req, res) => {
             const user = new User({ username, password });
             user.save().then(user => {
                 const token = makeToken(user);
-                res.status(201).json({ token });
+                res.status(201).json({ token, user: { _id: user._id, username: user.username }  });
             })
             .catch(err => {
                 res.status(500).json(err);
@@ -43,25 +38,3 @@ exports.login = (req, res) => {
     }
     res.status(200).json({ token: makeToken(req.user), user: req.user })
 }
-
-// // Logout
-// exports.logout = (req, res) => {
-//     if (req.session) {
-//         req.session.destroy(err => {
-//             if (err) res.status(500).json({ error: 'error logging out' });
-//             else res.status(200).send('Logged out');
-//         });
-//     } else {
-//         res.send('User not logged in');
-//     }
-// }
-
-// Update an exisiting user
-exports.update = (req, res) => {
-
-};
-
-// Delete an existing user
-exports.delete = (req, res) => {
-
-};
